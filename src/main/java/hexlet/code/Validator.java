50641cb4ec9270217585f959da48bb8e8cbd6d2d
oleg <olegmms@gmail.com>
2024-11-17 18:31:35 +0300

    fix(): unused imports was removed

--- src/main/java/hexlet/code/Validator.java
+++ src/main/java/hexlet/code/Validator.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
-import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
@@ -12,7 +10,6 @@ public class Validator {
     public StringSchema string() {
         return new StringSchema();
     }
-
 
     public NumberSchema number() {
         return new NumberSchema();
